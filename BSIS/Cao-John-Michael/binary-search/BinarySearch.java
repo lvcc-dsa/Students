4a3f0c4f5c728424b19eac733c1984d9e3bf874c
John Michael Cao <johnmichaelcao@yahoo.com>
2016-10-17 23:43:56 +0800

    Just had a hard time completing the code

--- BSIS/Cao-John-Michael/binary-search/BinarySearch.java
+++ BSIS/Cao-John-Michael/binary-search/BinarySearch.java
@@ -6,7 +6,7 @@
 * @link    [full github url]
 * @version [version number]
 * @since   [last updated dd/mm/yyyy]
-*/
+
 
 import java.util.*; // note: imports all classes inside java.util
 
@@ -70,4 +70,106 @@ public class BinarySearch {
 
 	}
 
-}
+}*/
+
+
+/* 
+* @author: [John Michael Cao]
+ * @link: [https://github.com/lvcc-dsa/Students/blob/master/BSIS/Cao-John-Michael/array-queue/ArrayQueue.java]
+ * @version: [final build]
+ * @since: [10/17/2016] 
+ */ 
+
+ 
+ import java.util.*; 
+ 
+ 
+ public class BinarySearch { 
+ 
+ 
+  static int array [], index, size, capacity;  
+  static int search,limit; 
+ 
+ 
+  public BinarySearch (int size) { 
+   capacity = size; 
+   array = new int [capacity]; 
+  } 
+ 
+  
+  public void sorted (int [] num) { 
+ 
+   int temp; 
+
+   for (int x = 0; x < num.length - 1; x++) { 
+    for (int y = 0; y < num.length - 1; y++) { 
+     if (num[y] > num[y+1]) { 
+      temp = num[y]; 
+      num[y] = num[y+1]; 
+      num[y+1] = temp; 
+     } 
+    } 
+   } 
+  } 
+ 
+ 
+  public int binSearch (int [] num, int find) { 
+   int left, mid, right; 
+ 
+   left = 0; 
+   right = num.length - 1; 
+    
+   while (right >= left) { 
+    mid = (left + right) / 2; 
+    if (num[mid] == find) { 
+     return 1; 
+    } 
+    if (num[mid] < find) { 
+     left = mid + 1; 
+    } 
+    if (num[mid] > find) { 
+     right = mid - 1;  
+    } 
+   } 
+   return -1; 
+  } 
+ 
+ 
+  public static void main(String [] args) { 
+ 
+   Scanner console = new Scanner(System.in); 
+   Random rand = new Random();  
+
+   System.out.print("Enter array size: "); 
+   size = console.nextInt(); 
+   System.out.println(); 
+ 
+   BinarySearch objClass = new BinarySearch(size); 
+  
+   System.out.print("Enter number to find: "); 
+   search = console.nextInt(); 
+   System.out.println(); 
+ 
+   System.out.println("ARRAY INPUTTED: "); 
+   for (int i = 0; i < size; i++) { 
+    objClass.array[i] = 1+(rand.nextInt(100)); 
+   } 
+ 
+   objClass.sorted(objClass.array); 
+ 
+   for (int i = 0; i < size; i++) { 
+    System.out.print("[" + objClass.array[i] + "] ");  
+   } 
+   System.out.println(); 
+ 
+   limit = objClass.binSearch(objClass.array, search); 
+ 
+   for (int i = 0; i < size; i++) { 
+     if (objClass.array[i] == search) { 
+       System.out.println(search + " is located at element: " + (i+1) + "."); 
+     }  else { 
+       System.out.println("NOT FOUND: " + search + " is not in the array."); 
+     } break;
+   }
+  } 
+ } 
