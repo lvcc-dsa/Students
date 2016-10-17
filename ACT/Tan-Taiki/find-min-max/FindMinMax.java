8700b97fa1fd14ed15a67cf0571fc3b75a143c0b
taikitan09 <taiki.ph.nakamura@gmail.com>
2016-10-18 00:00:13 +0800

    Add files via upload

--- ACT/Tan-Taiki/find-min-max/FindMinMax.java
+++ ACT/Tan-Taiki/find-min-max/FindMinMax.java
@@ -1,15 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findminmax;
+
+/**
+ *
+ * @author taiki nakamura
+ */
 public class FindMinMax {
-	public static void main(String[] args) {
-		int[] numbers = { 88, 33, 55, 23, 64, 123 };
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min)
-				min = numbers[i];
-			if (numbers[i] > max)
-				max = numbers[i];
-		}
-		System.out.println("Smallest number is " + min);
-		System.out.println("Lagest number is " + max);
-	}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] numbers = {88,33,55,23,64,123};
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length; i++){
+            if (max < numbers[i])
+                max = numbers[i];
+            if (min > numbers[i])
+                min = numbers[i];
+        }
+        System.out.println("Smallest number is " + min);
+        System.out.println("Largest number is " + max);
+    }
+    
 }
