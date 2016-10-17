435ae44eff66c54453bc2d7297bcf9ddd858acce
taikitan09 <taiki.ph.nakamura@gmail.com>
2016-10-17 23:56:02 +0800

    Add files via upload

--- ACT/Tan-Taiki/linear-search/LinearSearch.java
+++ ACT/Tan-Taiki/linear-search/LinearSearch.java
@@ -1,43 +1,45 @@
-/**
-* LINEAR SEARCH checks each item in a collection from start to end until a match is found. 
-*
-* @author  Renaldo Valente
-* @link    https://github.com/arrowbrave
-* @version 1.1
-* @since   22/09/2016
-*/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linearsearch;
 
+/**
+ *
+ * @author taiki nakamura
+ */
 import java.util.Scanner;
 import java.util.Random;
-
 public class LinearSearch {
 
-	public static void main(String[] args) {
-
-		int i, size, search, array[];
-
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Enter array size: ");
-		size = input.nextInt();
-		array = new int[size];		
-
-		System.out.print("Enter number to find: ");
-		search = input.nextInt();
-
-		for (i = 0; i < size; i++) {
-			array[i] = rand.nextInt(1000);
-			if (array[i] == search) {
-				System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
-				break;
-			} 			
-		}
-
-		if (i == size) {
-			System.out.println("NOT FOUND: " + search + " is not in the array.");
-		}
-
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int i, size, search, array[];
+      
+      Scanner input = new Scanner(System.in);
+      Random rand = new Random();
+      
+      System.out.print("Enter array size: ");
+      size = input.nextInt();
+      array = new int[size];
+      
+      System.out.print("Enter number to find: ");
+      search = input.nextInt();
+      
+      for (i = 0; i < size; i++){
+          array[i] = rand.nextInt(100);
+          if (array[i] == search) {
+           System.out.println("FOUND: " + search + " is at " + "array["+i+"]");
+           break;
+          }
+      }
+      if (i == size){
+          System.out.println("NOT FOUND: " + search + " is not in the array.");
+      }
+      
+    }
+    
 }
