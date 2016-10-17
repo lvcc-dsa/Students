a0a7829c92507f501fa310e0d660b1970fe4be5f
John Michael Cao <johnmichaelcao@yahoo.com>
2016-10-17 23:30:56 +0800

    Uploaded my previous code

--- BSIS/Cao-John-Michael/bubble-sort/BubbleSort.java
+++ BSIS/Cao-John-Michael/bubble-sort/BubbleSort.java
@@ -6,7 +6,7 @@
 * @link    [full github url]
 * @version [version number]
 * @since   [last updated dd/mm/yyyy]
-*/
+*
 
 // TODO: import Scanner and Random from Java library
 
@@ -50,4 +50,63 @@ public class BubbleSort {
 		// TODO: print sorted numbers separated by space
 	}
 
+}*/
+
+
+/** 
+ * BUBBLE SORT is based on the idea of repeatedly comparing pairs of  
+ * adjacent elements, then switching positions if they exist in the wrong order. 
+ * 
+ * @author  [John Michael Cao] 
+ * @link    [https://github.com/lvcc-dsa/Students/blob/master/BSIS/Cao-John-Michael/bubble-sort/BubbleSort.java] 
+ * @version [final build] 
+ * @since   [17/10/2016] 
+ */ 
+ 
+ 
+ // TODO: import Scanner and Random from Java library 
+
+import java.util.Scanner;
+import java.util.Random;
+public class BubbleSort {
+    
+  public static void main(String[] args) { 
+   
+    int size, x, y, swap, array[];
+    Scanner input = new Scanner(System.in);
+    
+    Random rand = new Random();
+    System.out.print("Enter array size to sort: ");
+    size = input.nextInt();
+    array = new int[size];
+    
+    System.out.println();
+    System.out.println(size + " Random number generated. ");
+                         
+            for(x = 0; x <size; x++){
+              array[x] = rand.nextInt(10000);
+              System.out.print(array[x] + " " );
+            }
+            
+            //Bubble Sorting process
+              System.out.println();
+               for(x = 0; x < size-1; x++){
+                    for(y = 0; y <size - x-1; y++){
+                         if(array[y] > array[y + 1]){
+                             swap = array[y];
+                             array[y] = array[y+1];
+                             array[y+1] = swap;
+                          }
+                    }
+               }
+               //Display
+               System.out.println();
+               System.out.println("Bubble sorting ...Done!");
+               for(x = 0; x < size; x++){
+                   System.out.print(array[x] + " ");
+               }
 }
+
+  }
+  
+
