8cde53557ef9c4ff66e19e9d475d7893769ef2dd
Ruthille Dagdag <rutheltan18@gmail.com>
2016-10-17 21:47:49 +0800

    Update ArrayStack.java

--- BSIS/Dagdag-Ruthille/array-stack/ArrayStack.java
+++ BSIS/Dagdag-Ruthille/array-stack/ArrayStack.java
@@ -67,12 +67,12 @@ public class ArrayStack {
     } else {      
       storage[top]; 
       top;
-      System.out.println(storage[top] + " was successfully removed."); // todo: what does pop do?
+      System.out.println(storage[top] + " was successfully removed."); 
       System.out.println();
     }
   }
 
-  // note: will show the topmost value of the array
+  
   public void peek() {
     if (isEmpty()) { 
       System.out.println("PEEK TOP = NULL");
@@ -84,36 +84,40 @@ public class ArrayStack {
   }
   
   public static void main(String[] args) {
-    // construct a new array queue and assign 10 as the integer value for the size limit of the stack
-    System.out.println("STORAGE CAPACITY = " + /* return the size limit */);
-    System.out.println();
-    // show empty stack
-    // try removing on an empty stack
-    // peek top element of an empty stack
-    storage.push("one");
-    // show the updated stack
-    // peek if top element is "one"
-    storage.push("two");
-    // show the updated stack
-    // peek if top element is "two"
-    storage.push("three");
-    // show the updated stack
-    // peek if top element is "three"
-    storage.push("four");
-    // show the updated stack
-    storage.push("five"); 
-    // show the updated stack
-    // try removing "five"
-    storage.push("six"); 
-    storage.push("seven"); 
-    storage.push("eight"); 
-    storage.push("nine");    
-    storage.push("ten"); 
-    // show the updated stack
-    // peek if top element is "ten"
-    // try adding "eleven"
-    // try adding "twelve"
-    // show the updated stack
+   
+		ArrayStack storage = new ArrayStack(10);
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("one");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("two");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("three");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("four");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("five");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("six");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("seven");
+		storage.show();
+		storage.pop();
+		storage.peek();
+		storage.push("eight");
+		storage.show();
   }  
 
-}
